1e8cb682fd526f8cf34a7bc4a67299e6e9619957
pakmarina108@gmail.com <pm@mail.ru>
2024-10-14 23:52:20 +0300

    added greeting on /start command

--- telewbot/services/service.py
+++ telewbot/services/service.py
@@ -8,10 +8,23 @@ from telewbot.credentials import tele_url, bot_token
 def parse_msg(msg):
     #get msg id and city
     chat_id = msg['message']['chat']['id']
-    city_orig = msg['message']['text']
-    #check for whitespaces
-    city = city_orig.strip()
-    #get weather depending on city
+    city = msg['message']['text']
+    #check if text msg is bot command /start
+    if city=="/start":
+        resp = "Чтобы узнать погоду, введите название города на английском языке."
+    else:    
+        #get weather depending on city
+        resp = get_external_weather(city)
+    return chat_id, resp
+
+def send_msg(chat_id, text):
+    url = f"{tele_url}/bot{bot_token}/sendMessage"
+    answer = {'chat_id':chat_id, 'text':text}
+    
+    r=requests.post(url, json=answer)
+    return r
+
+def get_external_weather(city):
     data = get_weather(city)
     if data:
         # Deserialize JSON into WeatherResponse object
@@ -23,11 +36,4 @@ def parse_msg(msg):
         resp = f"В г.{city_name} {weather}\nTемпература: {str(temperature)} \nСкорость ветра: {wspeed}"
     else:
         resp = ""
-    return chat_id, resp
-
-def send_msg(chat_id, text):
-    url = f"{tele_url}/bot{bot_token}/sendMessage"
-    answer = {'chat_id':chat_id, 'text':text}
-    
-    r=requests.post(url, json=answer)
-    return r
+    return resp
